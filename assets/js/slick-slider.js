8e45fbfacf985c0c677d6be8627856bf8205a2d1
TomVito <t.vitonis@gmail.com>
2021-06-18 20:19:42 +0300

    slick slider

---
+++ assets/js/slick-slider.js
@@ -0,0 +1,26 @@
+$(document).ready(function () {
+  $(".slider1").slick({
+    infinite: true,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    dots: true,
+    autoplay: true,
+    autoplaySpeed: 2000,
+  });
+  $(".slider2").slick({
+    infinite: true,
+    slidesToShow: 2,
+    slidesToScroll: 1,
+    dots: true,
+    autoplay: true,
+    autoplaySpeed: 2000,
+  });
+  $(".slider3").slick({
+    infinite: true,
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    dots: true,
+    autoplay: true,
+    autoplaySpeed: 2000,
+  });
+});
